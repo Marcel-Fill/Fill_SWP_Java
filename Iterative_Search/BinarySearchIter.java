c669a4751336da8a94cad03f6be48622bf936fb2
Marcel-Fill <you@example.com>
2021-10-07 10:34:24 +0200

    Update BinarySearchIter.java

--- Iterative_Search/BinarySearchIter.java
+++ Iterative_Search/BinarySearchIter.java
@@ -1,8 +1,13 @@
 package Iterative_Search;
 
+
+
 class BinarySearchIter {
     // Returns index of x if it is present in arr[],
     // else return -1
+    static int counter = 0;
+
+
     int binarySearch(int arr[], int x)
     {
         int l = 0, r = arr.length - 1;
@@ -29,9 +34,10 @@ class BinarySearchIter {
   
     // Driver method to test above
     public static void main(String args[])
-    {
+    {   
         BinarySearchIter ob = new BinarySearchIter();
-        int arr[] = { 2, 3, 4, 10, 40 };
+        int arr[] = arrSearch(100);
+        giveArr(arr);
         int n = arr.length;
         int x = 10; //starting number 
         int result = ob.binarySearch(arr, x);
@@ -40,5 +46,21 @@ class BinarySearchIter {
         else
             System.out.println("Element found at "
                                + "index " + result);
+    }
+
+
+    public static int[] arrSearch(int yeet){
+    int arr[] = new int[yeet];
+    for(int i = 0; i < arr.length; i++){
+        arr[i] = i +1;
+    }
+    return arr; 
+    }
+
+    public static void giveArr(int[] arr2){
+        for(int i = 0; i < arr2.length; i++ ) {
+            System.out.println(arr2[i]);
+        }
+        
     }
 }
