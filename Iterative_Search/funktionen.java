16e857dc02693bc5e08e2e6ab0965acf0a3a1e3f
Marcel-Fill <you@example.com>
2021-11-03 18:27:03 +0100

    Update funktionen.java

--- Iterative_Search/funktionen.java
+++ Iterative_Search/funktionen.java
@@ -1,24 +1,37 @@
 package Iterative_Search;
 
 public class funktionen {
-  public int rsum;   
 
 
     public static void main(String[] args) {
-        int test = summe(5);
+        int test = powfak(2,5);
       System.out.println(test); 
     }
 
 
 public static int summe(int sum){
-   
-    if(sum < 1) {
+   int rsum = 0; 
+    if(sum <= 1) {
+        return 0;
+    } else {
+        return sum += summe(sum -1);    //to auto take the number before 
+
+    }
+}
+
+public static int fak(int fakt){
+    if(fakt <= 1){
         return 1;
     } else {
-        for(int i = sum; i >= 0; i-- ){
-            rsum = rsum + sum;
-        }
-        return sum;
+        return fakt *= fak(fakt -1);
+    }
+}
+
+public static int powfak(int base, int expo){
+    if(expo  <= 1){
+        return 1;
+    } else {
+        return base *= powfak(base,expo -1 );
     }
 }
 }
