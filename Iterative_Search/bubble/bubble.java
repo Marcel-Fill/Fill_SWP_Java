41ef658b20c9882bb28a71b5c9557b1987e3e5fd
Marcel-Fill <you@example.com>
2021-12-09 10:38:43 +0100

    Update bubble.java

--- Iterative_Search/bubble/bubble.java
+++ Iterative_Search/bubble/bubble.java
@@ -5,6 +5,17 @@ import java.lang.reflect.Array;
 
 class bubble
 {
+    public static void main(String args[])
+    {
+        long t1 = System.nanoTime();
+        bubble ob = new bubble();
+        int arr[] = randomArray(100000,1000);
+        ob.bubbleSort(arr);
+        ob.printArray(arr);
+        long t2 = System.nanoTime();
+        System.out.println("Sorted Version took for: "+ ((t2-t1)/1000) + " mikro seks | " + ((t2-t1)/1000000)+"ms" );
+    }
+
     void bubbleSort(int arr[])
     {
         int n = arr.length;
@@ -21,7 +32,7 @@ class bubble
     void printArray(int arr[])
     {
         int n = arr.length;
-        for (int i=0; i<n; ++i)
+        for (int i=0; i<n;++i)
             System.out.print(arr[i] + " ");
         System.out.println();
     }
@@ -36,14 +47,5 @@ class bubble
         return arr; 
     }
  
-    public static void main(String args[])
-    {
-        long t1 = System.nanoTime();
-        bubble ob = new bubble();
-        int arr[] = randomArray(1000,1000);
-        ob.bubbleSort(arr);
-        ob.printArray(arr);
-        long t2 = System.nanoTime();
-        System.out.println("Sorted Version took for: "+ ((t2-t1)/1000) + " mikro seks | " + ((t2-t1)/1000000)+"ms" );
-    }
+    
 }
